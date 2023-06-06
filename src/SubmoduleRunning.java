bc18628bf49e37f12e3555d865df7d2c70d1f975
ITD\Kazlou_MS <mister.nikita2000@bk.ru>
2023-06-06 12:51:33 +0300

    Second submodule commit

--- src/SubmoduleRunning.java
+++ src/SubmoduleRunning.java
@@ -1,5 +1,9 @@
+import java.util.LinkedList;
+
 public class SubmoduleRunning {
     public static void main(String[] args) {
         System.out.println("Submodule is running!!!");
+
+        LinkedList<Integer> linkedList = new LinkedList();
     }
 }
